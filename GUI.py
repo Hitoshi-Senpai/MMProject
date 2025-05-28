2eadf41ef290ef110fd4e76de7adaea0bb9822be
mazen <mazenaltabbaa366@gmail.com>
2025-05-29 01:33:06 +0300

    Create two GUI windows: the first one is a HomePage and the second is an ImageSelectPage (which will later call the ModePage).

--- GUI.py
+++ GUI.py
@@ -16,9 +16,11 @@ class GUI:
         self.style = tb.Style()
         self.style.configure('TNotebook.Tab', font=('Helvetica', '12'), padding=[20, 10])
         self.pathTemp = tk.StringVar()
-        self.createPage()
+        self.HomePage()
         
     def createPage(self):
+
+        self.__init__(root)
         self.notebook = tb.Notebook(self.root, bootstyle="dark")
         self.notebook.pack(pady=20, fill=tk.BOTH, expand=True)
         
@@ -39,20 +41,12 @@ class GUI:
         iconLabel = tk.Label(self.tab1, image=self.icon1)
         iconLabel.pack(pady=10)
 
-        self.style.configure(
-            'LargeFont.TButton',
-            font=('Helvetica', 20),
-            padding=(0, 5),
-            anchor='center'
-        )
-
         self.selectBtn1=tk.Button(
             self.tab1, 
             text="generate a random Image", 
             width=20,
             height=8,
             font=("Helvetica", 15),
-            # state='LargeFont.TButton',
             command=self.openFolder
         )
         self.selectBtn1.place(x=130, y=200)
@@ -61,10 +55,8 @@ class GUI:
         self.selectBtn2=tk.Button(
             self.tab1, 
             text="Choose an image from disk",
-            # bootstyle="success",
             width=20,
             height= 8,
-            # state='LargeFont.TButton',
             font=("Helvetica", 15),
             command=self.openFolder
         )
@@ -74,6 +66,41 @@ class GUI:
         self.label1 =tk.Label(self.tab1, text="Image Selection Page", font=("Helvetica", 18))
         self.label1.pack(pady=10)
 
+    def HomePage(self):
+        self.greeting =tk.Label(root, text="Welcome to our game", font=("MV Boli", 22))
+        self.greeting.place(x=450, y=50)
+
+        self.developers =tk.Label(root, text="Developed by Mazen, Ahmad and Mohammad", font=("MV Boli", 15))
+        self.developers.place(x=10, y=630)
+
+        self.icon1 = tk.PhotoImage(file="icons/gear4.gif")
+        self.iconLabel1 = tk.Label(root, image=self.icon1)
+        self.iconLabel1.place(x=150, y=150)
+
+        self.icon2 = tk.PhotoImage(file="icons/gear3.gif")
+        self.iconLabel2 = tk.Label(root, image=self.icon2)
+        self.iconLabel2.place(x=1000, y=550)
+
+
+        self.startBtn=tk.Button(
+            root, 
+            text="Start", 
+            width=14,
+            height=1,
+            font=("MV Boli", 17),
+            command= self.createPage
+        )
+        self.startBtn.place(x=550, y=300)
+
+        self.exitBtn=tk.Button(
+            root, 
+            text="Exit", 
+            width=14,
+            height=1,
+            font=("MV Boli", 17),
+            command= None
+        )
+        self.exitBtn.place(x=550, y=400)
 
 
     def openFolder(self):
@@ -87,8 +114,7 @@ class GUI:
             self.textArea.insert(tk.END, f"Selected Image Path:\n{self.image}\n\n")
             self.textArea.insert(tk.END, f"Image successfully selected (:")
             self.pathTemp.set(self.image)
-
-
+    
 
 root = tb.Window(themename="superhero")
 app = GUI(root)
