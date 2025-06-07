6043ace7f46aa782910dc57b16d20af30bff60f3
mazen <mazenaltabbaa366@gmail.com>
2025-06-07 22:23:07 +0300

    I created a new directory called AutoImages for use in the second mode. I added three new functions to Game.py: findDifferencesAuto, getAutoDifflen, and compareWithAuto, along with a new variable called autoDifferences. In GUI.py, I added an isAuto flag to enable the second mode and modified all relevant functions and buttons to support execution in this new mode (:

--- Game.py
+++ Game.py
@@ -1,5 +1,8 @@
 import random
 import pygame
+import cv2
+import numpy as np
+
 
 
 class Game:
@@ -11,6 +14,7 @@ class Game:
         self.effectPath = ""
         self.silent = False
         self.disableBackOst = False
+        self.autoDifferences = []
 
     def storeRanges(self):
         self.imgsRange.append([[(235, 150), (285, 222)], [(100, 350), (130, 375)], [(30, 470),(85, 433)], [(474, 465), (510, 415)], [(470, 80), (550, 130)],[(100, 170),(150, 190)]])
@@ -60,6 +64,53 @@ class Game:
             if not inRange:
                 missing.append(origin)
         return (score, missing)
+    
+
+    def findDifferencesAuto(self, orgPath, missPath):
+
+        org = cv2.imread(orgPath)
+        miss = cv2.imread(missPath)
+        
+        grayOrg = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
+        grayMiss = cv2.cvtColor(miss, cv2.COLOR_BGR2GRAY)
+        
+        diff = cv2.absdiff(grayOrg, grayMiss)
+        
+        _, threshold = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+        diffs, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        for diff in diffs:
+            x, y, newx, newy = cv2.boundingRect(diff)
+            
+            if newx > 5 and newy > 5:
+                self.autoDifferences.append([(x, y), (x+newx, y+newy)])
+
+    def getAutoDiffLen(self):
+        return len(self.autoDifferences)
+    
+    def compareWithAuto(self, playerRanges):
+        score = 0
+        missing = []
+        
+        for origin in self.autoDifferences:
+            ostart, oend = origin
+            inRange= False
+            
+            for player in playerRanges:
+                pstart, pend = player
+                
+                if not (pend[0] < ostart[0] or  
+                        pstart[0] > oend[0] or   
+                        pend[1] < ostart[1] or 
+                        pstart[1] > oend[1]): 
+                    score += 1
+                    inRange = True
+                    break
+                    
+            if not inRange:
+                missing.append(origin)
+        return (score, missing)
+        
     
     def playBackgroundOst(self):
         if self.disableBackOst:
